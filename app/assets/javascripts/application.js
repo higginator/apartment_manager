b2ea31808ba3df36d7baa0724b404b208aa39707
Ryan Higgins <ryanhigs@gmail.com>
2015-06-30 13:48:46 -0700

    navigation bar now adding padding to body when the navigation bar is resized

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,74 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+	var isDropdownOpen = false;
+	var initially = true;
+	$('.dropdown').click(function() {
+		if (initially) {
+			isDropdownOpen = true;
+			initially = false;
+			initialPaddingHandler();
+		} else {
+			handleNavBarToBodyPadding();
+		}
+	});
+
+	var initialPaddingHandler = function() {
+		if (isDropdownOpen) {
+			if ($(window).width() < 768) {
+				$('body').addClass('dropdownIsOpenPadding');
+			} else {
+				//dont add padding
+			}
+		}
+	}
+
+	var handleNavBarToBodyPadding = function() {
+		if (isDropdownOpen) {
+			//navbar is open and about to close, so remove the padding if it exists
+			if ($('body').hasClass('dropdownIsOpenPadding')) {
+				$('body').removeClass('dropdownIsOpenPadding');
+			}
+		} else {
+			//navbar is not open and about to open, so add the padding if needed
+			if ($(window).width() < 768) {
+				$('body').addClass('dropdownIsOpenPadding');
+			} else {
+				//dont add padding
+			}
+		}
+		toggleIsDropDownOpen();
+	};
+
+	var toggleIsDropDownOpen = function() {
+		isDropdownOpen = !isDropdownOpen;
+	}
+
+	$(window).resize(function() {
+		//if user resizes browser to greater than 768 AND the dropdown padding is added, then remove the padding
+		if (($(window).width() >= 768) && ($('body').hasClass('dropdownIsOpenPadding'))) {
+			$('body').removeClass('dropdownIsOpenPadding');
+		};
+		//if user resizes browser to less than 768 AND dropdown is open AND dropdown padding is not added, then add the padding
+		if ($(window).width() < 768) {
+			if (isDropdownOpen) {
+				if (!($('body').hasClass('dropdownIsOpenPadding'))) {
+					$('body').addClass('dropdownIsOpenPadding');
+				}
+			}
+		}
+	});
+});
+
+
+
+
+
+
